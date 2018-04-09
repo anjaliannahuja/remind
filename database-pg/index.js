51851830eb68721a53b86a1b543e81fb615728f4
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-04-08 23:30:21 -0500

    Changed to database url

--- database-pg/index.js
+++ database-pg/index.js
@@ -1,11 +1,6 @@
 var pg = require('pg');
 
-var connection = ({
-  host     : 'localhost',
-  user     : 'anjaliahuja',
-  password : '',
-  database : 'twilio'
-});
+var connection = (process.env.DATABASE_URL);
 
 const client = new pg.Client(connection);
 client.connect();
